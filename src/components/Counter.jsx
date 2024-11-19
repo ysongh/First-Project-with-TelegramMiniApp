1a7103249627342641cacf0be2aefc510cb79cea
You Song Hou <ysongweb3@gmail.com>
2024-11-19 01:24:21 -0500

    feat: integrate Tailwind CSS

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,7 @@ function Counter() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>
+      <button className='bg-blue-300 p-2 rounded-md' onClick={() => setCount(count + 1)}>
         Add
       </button>
       <p>{count}</p>
